9a647796b345b0ca749cca8360b52bdb60c7e19f
Ali Sayar <alisayar07@gmail.com>
2023-08-01 12:11:56 +0300

    LoginView

--- lib/screen/auth/login/login_view.dart
+++ lib/screen/auth/login/login_view.dart
@@ -1,6 +1,6 @@
 import 'package:ecommerce/core/base/view/base_view.dart';
-import 'package:ecommerce/core/components/app_progress.dart';
 import 'package:ecommerce/core/components/gradiant_box.dart';
+import 'package:ecommerce/core/components/text_field/custom_text_field.dart';
 import 'package:ecommerce/screen/auth/login/viewmodel/login_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
@@ -36,7 +36,8 @@ class _LoginViewState extends BaseState<LoginView> {
         onTap: (() => FocusScope.of(context).unfocus()),
         child: Stack(
           children: <Widget>[
-            GradiantBox(a: themeData.primaryColor, b: themeData.colorScheme.secondary),
+            GradiantBox(
+                a: themeData.primaryColor, b: themeData.colorScheme.secondary),
             SingleChildScrollView(
               padding:
                   const EdgeInsets.symmetric(vertical: 140, horizontal: 20),
@@ -67,25 +68,10 @@ class _LoginViewState extends BaseState<LoginView> {
                                       color:
                                           themeData.colorScheme.onBackground))),
                           child: Padding(
-                            padding: EdgeInsets.symmetric(vertical: 8.0),
+                            padding: const EdgeInsets.symmetric(vertical: 8.0),
                             child: Observer(
                               builder: (BuildContext context) {
-                                return TextField(
-                                  onChanged: (value) {
-                                    if(value != ""){
-                                      viewModel.emailError = false;
-                                    } else {
-                                      viewModel.emailError = true;
-                                    }
-                                  },
-                                  controller: viewModel.emailController,
-                                  decoration: InputDecoration(
-                                      prefixIcon: Icon(Icons.mail_outline),
-                                      border: InputBorder.none,
-                                      hintText: "E-posta ya da telefon",
-                                      errorText: viewModel.emailError == true ? "E-posta adresinizi giriniz!" : null
-                                  ),
-                                );
+                                return buildEmailCustomTextField();
                               },
                             ),
                           ),
@@ -94,93 +80,16 @@ class _LoginViewState extends BaseState<LoginView> {
                           padding: const EdgeInsets.symmetric(vertical: 8.0),
                           child: Observer(
                             builder: (_) {
-                              return TextField(
-                                onChanged: (value){
-                                  if(value != ""){
-                                    viewModel.passwordError = false;
-                                  } else {
-                                    viewModel.passwordError = true;
-                                  }
-                                },
-                                controller: viewModel.passwordController,
-                                obscureText: !viewModel.passwordVisibility,
-                                decoration: InputDecoration(
-                                  prefixIcon: const Icon(Icons.lock_outline),
-                                  border: InputBorder.none,
-                                  hintText: "Parola",
-                                  errorText: viewModel.passwordError == true ? "Parolanızı giriniz!": null,
-                                  suffixIcon: IconButton(
-                                    icon: Icon(
-                                      viewModel.passwordVisibility != true ? Icons.visibility : Icons.visibility_off
-                                    ),
-                                    onPressed: () {
-                                      viewModel.passwordVisibility = !viewModel.passwordVisibility;
-                                    },
-                                  )
-                                ),
-                              );
+                              return buildPasswordCustomTextField();
                             },
                           ),
                         ),
-                        Container(
-                          alignment: Alignment.centerRight,
-                          child: TextButton(
-                            onPressed: () {
-                              Navigator.of(context)
-                                  .pushNamed("/forgot_password_view");
-                            },
-                            child: const Text("Parolanızı mı unuttunuz?"),
-                          ),
-                        )
+                        buildForgotPasswordContainer()
                       ],
                     ),
                   ),
-                  Container(
-                    alignment: Alignment.center,
-                    margin:
-                        const EdgeInsets.symmetric(horizontal: 0, vertical: 25),
-                    child: ElevatedButton(
-                      onPressed: (){
-                        viewModel.login();
-                      },
-                      style: ButtonStyle(
-                          padding: MaterialStateProperty.all(
-                              const EdgeInsets.all(15)),
-                          shape: MaterialStateProperty.all(
-                              const RoundedRectangleBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(15)),
-                          )),
-                          backgroundColor: MaterialStateProperty.all(
-                              themeData.colorScheme.secondary),
-                          shadowColor: MaterialStateProperty.all(
-                              themeData.colorScheme.secondary)),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: const <Widget>[
-                          Text("Giriş Yap  "),
-                          Icon(Icons.login_outlined)
-                        ],
-                      ),
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      const Text(
-                        "Henüz bir hesabınız yok mu?",
-                        style: TextStyle(color: Colors.white70),
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          viewModel.goRegisterView();
-                        },
-                        child: const Text(
-                          "Kayıt Olun!",
-                          style: TextStyle(color: Colors.white70),
-                        ),
-                      )
-                    ],
-                  ),
+                  buildLoginButtonContainer(),
+                  buildRegisterRow(),
                   Container(
                       alignment: Alignment.center,
                       margin: const EdgeInsets.symmetric(vertical: 15),
@@ -197,14 +106,116 @@ class _LoginViewState extends BaseState<LoginView> {
     );
   }
 
+  Row buildRegisterRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        const Text(
+          "Henüz bir hesabınız yok mu?",
+          style: TextStyle(color: Colors.white70),
+        ),
+        TextButton(
+          onPressed: () {
+            viewModel.goRegisterView();
+          },
+          child: const Text(
+            "Kayıt Olun!",
+            style: TextStyle(color: Colors.white70),
+          ),
+        )
+      ],
+    );
+  }
+
+  Container buildLoginButtonContainer() {
+    return Container(
+      alignment: Alignment.center,
+      margin: const EdgeInsets.symmetric(horizontal: 0, vertical: 25),
+      child: ElevatedButton(
+        onPressed: () {
+          viewModel.login();
+        },
+        style: ButtonStyle(
+            padding: MaterialStateProperty.all(const EdgeInsets.all(15)),
+            shape: MaterialStateProperty.all(const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(15)),
+            )),
+            backgroundColor:
+                MaterialStateProperty.all(themeData.colorScheme.secondary),
+            shadowColor:
+                MaterialStateProperty.all(themeData.colorScheme.secondary)),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text("Giriş Yap  "),
+            Icon(Icons.login_outlined)
+          ],
+        ),
+      ),
+    );
+  }
+
+  Container buildForgotPasswordContainer() {
+    return Container(
+      alignment: Alignment.centerRight,
+      child: TextButton(
+        onPressed: () {
+          Navigator.of(context).pushNamed("/forgot_password_view");
+        },
+        child: const Text("Parolanızı mı unuttunuz?"),
+      ),
+    );
+  }
+
+  CustomTextField buildPasswordCustomTextField() {
+    return CustomTextField(
+        onChanged: (value) {
+          if (value != "") {
+            viewModel.passwordError = false;
+          } else {
+            viewModel.passwordError = true;
+          }
+        },
+        controller: viewModel.passwordController,
+        obscureText: !viewModel.passwordVisibility,
+        prefixIcon: const Icon(Icons.lock_outline),
+        errorText: "Parolanızı giriniz!",
+        error: viewModel.passwordError,
+        hintText: "Parola",
+        suffixIcon: IconButton(
+          icon: Icon(viewModel.passwordVisibility != true
+              ? Icons.visibility
+              : Icons.visibility_off),
+          onPressed: () {
+            viewModel.passwordVisibility = !viewModel.passwordVisibility;
+          },
+        ));
+  }
+
+  CustomTextField buildEmailCustomTextField() {
+    return CustomTextField(
+      controller: viewModel.emailController,
+      hintText: "E-posta ya da telefon",
+      prefixIcon: Icon(Icons.email_outlined),
+      errorText: "E-posta adresinizi giriniz.",
+      error: viewModel.emailError,
+      obscureText: false,
+      onChanged: (value) {
+        if (value != "") {
+          viewModel.emailError = false;
+        } else {
+          viewModel.emailError = true;
+        }
+      },
+    );
+  }
+
   Text get buildHelloText {
     return const Text(
-                  "Merhaba",
-                  style: TextStyle(
-                      fontSize: 50,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w200),
-                  textAlign: TextAlign.start,
-                );
+      "Merhaba",
+      style: TextStyle(
+          fontSize: 50, color: Colors.white, fontWeight: FontWeight.w200),
+      textAlign: TextAlign.start,
+    );
   }
 }
