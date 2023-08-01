ca8ece83592379288b0f50004f7450872caa65e4
Ali Sayar <alisayar07@gmail.com>
2023-08-01 09:28:17 +0300

    register route constant

--- lib/core/constants/navigation/navigation_constants.dart
+++ lib/core/constants/navigation/navigation_constants.dart
@@ -2,4 +2,5 @@ class NavigationConstants {
   static const DEFAULT = "/";
   static const SPLASH_SCREEN = "/splash_screen";
   static const LOGIN_VIEW = "/login_view";
+  static const REGISTER_VIEW = "/register_view";
 }
