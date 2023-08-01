8c376ba6085dbbf96e0e81e2dca566839789c453
Ali Sayar <alisayar07@gmail.com>
2023-08-01 09:28:08 +0300

    register route

--- lib/core/init/navigation/navigation_route.dart
+++ lib/core/init/navigation/navigation_route.dart
@@ -1,5 +1,6 @@
 import 'package:ecommerce/core/constants/navigation/navigation_constants.dart';
 import 'package:ecommerce/screen/auth/login/login_view.dart';
+import 'package:ecommerce/screen/auth/register/register_view.dart';
 import 'package:ecommerce/screen/auth/splash/splash_view.dart';
 import 'package:flutter/material.dart';
 
@@ -16,6 +17,8 @@ class NavigationRoute {
         return normalNavigate(const SplashView());
       case NavigationConstants.LOGIN_VIEW:
         return normalNavigate(const LoginView());
+      case NavigationConstants.REGISTER_VIEW:
+        return normalNavigate(const RegisterView());
       default:
         return MaterialPageRoute(
             builder: (context) => const Scaffold(
