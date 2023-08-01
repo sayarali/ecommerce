2163afc23c9885274474fbf287a398d58541f54a
Ali Sayar <alisayar07@gmail.com>
2023-08-01 09:17:38 +0300

    SignUp service

--- lib/core/service/firebase_service.dart
+++ lib/core/service/firebase_service.dart
@@ -32,6 +32,33 @@ class FirebaseService {
     }
   }
 
+  //Sign Up
+  Future<User> signUp(String email, String password) async {
+    try {
+      UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
+        email: email.trim(),
+        password: password.trim(),
+      );
+      return userCredential.user;
+    } on FirebaseAuthException catch (e) {
+      String errorMessage;
+      if (e.code == 'email-already-in-use') {
+        errorMessage = 'Bu e-posta adresi zaten kullanılıyor.';
+      } else if (e.code == 'weak-password') {
+        errorMessage = 'Zayıf bir şifre. Daha güçlü bir şifre seçin.';
+      } else if (e.code == 'invalid-email') {
+        errorMessage = 'Geçersiz e-posta adresi.';
+      } else if (e.code == 'network-request-failed') {
+        errorMessage = 'İnternet bağlantınızda bir sorun oluştu.';
+      } else {
+        errorMessage = 'Kayıt oluşturulamadı. Hata kodu: ${e.code}';
+      }
+      throw errorMessage;
+    } catch (e) {
+      throw Exception('Kayıt oluşturulamadı: $e');
+    }
+  }
+
   //Sign Out
   Future<void> signOut() async {
     await _auth.signOut();
